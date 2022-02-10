3469eede1bf23d1c50096a98bdcedd56aa35c5f3
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2022-02-10 13:21:16 +0700

    CRUD of Hospitals database

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 
-//Connect to database
-connectDB();
-
 // Routes
 const hospitals = require('./routes/hospitals');
 
@@ -13,13 +10,19 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+//Connect to database
+connectDB();
+
+//Body praser
+app.use(express.json());
+
 app.use('/api/v1/hospitals', hospitals);
 
 const PORT = process.env.PORT | 5000;
 
 const server = app.listen(
 	PORT,
-	console.log('Server running in ', process.env.NODE_ENV, 'mode on port ', PORT)
+	console.log('Server running in', process.env.NODE_ENV, 'mode on port', PORT)
 );
 
 process.on('unhandleRejection', (err, promise) => {
