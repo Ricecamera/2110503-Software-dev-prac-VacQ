163633c1a869f86a5165aff44a109e6720697b3e
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2022-03-15 14:08:14 +0700

    Add pagination for `getHospitals`

--- controllers/hospitals.js
+++ controllers/hospitals.js
@@ -4,20 +4,76 @@ const Hospital = require('../models/Hospital');
 //@accress  Public
 exports.getHospitals = async (req, res, next) => {
 	let query;
-	let queryStr = JSON.stringify(req.query);
+
+	//Copy req.query data
+	const reqQuery = { ...req.query };
+
+	//Fields to exclude
+	const removeFields = ['select', 'sort', 'page', 'limit'];
+
+	//Loop over remove fields and delete them from reqQuery
+	removeFields.forEach(param => delete reqQuery[param]);
+	console.log(reqQuery);
+
+	let queryStr = JSON.stringify(reqQuery);
 	queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
 
 	query = Hospital.find(JSON.parse(queryStr));
 
+	// Select Fields
+	if (req.query.select) {
+		const fields = req.query.select.split(',').join(' ');
+		query = query.select(fields);
+	}
+
+	//Sort
+	if (req.query.sort) {
+		const sortBy = req.query.sort.split(',').join(' ');
+		query = query.sort(sortBy);
+	} else {
+		query = query.sort('-createAt');
+	}
+
+	//Pagination
+	const page = parseInt(req.query.page, 10) || 1;
+	const limit = parseInt(req.query.limit, 10) || 25;
+	const startIndex = (page - 1) * limit;
+	const endIndex = page * limit;
+	const total = await Hospital.countDocuments();
+
+	query = query.skip(startIndex).limit(limit);
+
+	//Executing query
 	try {
 		const hospitals = await query;
-		console.log(req.query);
+		//console.log(req.query);
+
+		//Pagination result
+		const pagination = {};
+
+		if (endIndex < total) {
+			pagination.next = {
+				page: page + 1,
+				limit,
+			};
+		}
+
+		if (startIndex > 0) {
+			pagination.prev = {
+				page: page - 1,
+				limit,
+			};
+		}
 
 		res
 			.status(200)
-			.json({ success: true, count: hospitals.length, data: hospitals });
+			.json({
+				success: true,
+				count: hospitals.length,
+				pagination,
+				data: hospitals,
+			});
 	} catch (err) {
-		console.log(err.message);
 		res.status(400).json({ success: false });
 	}
 };
